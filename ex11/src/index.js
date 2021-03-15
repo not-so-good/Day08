ddcb00245d2a1e14baf10cf7c3acbb7d68951365
not-so-good <sanjin.suljanovic@gmail.com>
2021-03-15 18:05:18 +0100

    ex11 Fractions - Generate random

--- ex11/src/index.js
+++ ex11/src/index.js
@@ -14,13 +14,13 @@ function mathTest(x,y) {
     
 }
 
-//var result = mathTest(2,2);
-//var result = mathTest(-2,2);
-//var result = mathTest(2,-2);
-//var result = mathTest(2,8);
-//var result = mathTest(3,3);
-var result = mathTest(0,0);
-console.log(result);
+console.log(mathTest(2,2));
+console.log(mathTest(-2,2));
+console.log(mathTest(2,-2));
+console.log(mathTest(2,8));
+console.log(mathTest(3,3));
+console.log(mathTest(0,0));
+
 
 //Only change code above this line
 
