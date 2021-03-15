3fe067f0ca4216525184ec3045d07e01faeb5a05
not-so-good <sanjin.suljanovic@gmail.com>
2021-03-15 17:01:38 +0100

    ex12 Functions - Generate random whole numbers

---
+++ ex12/src/index.js
@@ -0,0 +1,10 @@
+//Only change code below this line
+function randomInteger() { 
+    return Math.floor(Math.random() * 30);
+}
+randomInteger();
+var random = randomInteger();
+console.log(random);
+//Only change code above this line
+
+module.exports = randomInteger;
