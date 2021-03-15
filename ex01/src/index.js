c31c876dee19727ba1b33060cc9cf8046dc4ac80
not-so-good <sanjin.suljanovic@gmail.com>
2021-03-15 10:49:04 +0100

    ex01 Introducing functions

---
+++ ex01/src/index.js
@@ -0,0 +1,11 @@
+//Write code bolve this line
+function reusableFunction() {
+    console.log("Hi Padawan!");
+}
+
+reusableFunction();
+//Write code above this line
+
+module.exports = reusableFunction;
+
+
